3dddabcef215d37504449f6feedf68b82fa2d853
Andressa <115846058+andressa-l@users.noreply.github.com>
2024-03-14 14:49:27 -0300

    💄feat: alterando header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,15 +2,45 @@ import { Link } from "react-router-dom";
 import "./_style.scss";
 import "react";
 import { useAuthContext } from "../../context/AuthContext";
+import { useEffect } from "react";
 
 export default function Header() {
   const { logoutAuthContext } = useAuthContext();
 
+  useEffect(() => {
+    const icon = document.getElementById("icon");
+    const icon1 = document.getElementById("a");
+    const icon2 = document.getElementById("b");
+    const icon3 = document.getElementById("c");
+    const nav = document.getElementById("nav");
+    const blue = document.getElementById("blue");
+
+    const handleClick = () => {
+      icon1.classList.toggle("a");
+      icon2.classList.toggle("c");
+      icon3.classList.toggle("b");
+      nav.classList.toggle("show");
+      blue.classList.toggle("slide");
+    };
+
+    icon.addEventListener("click", handleClick);
+
+    return () => {
+      icon.removeEventListener("click", handleClick);
+    };
+  }, []);
+
   return (
     <>
-      <header className="flex-space-between">
-        <nav>
-          <ul className="flex-space-between g-2">
+      <header className="header-hamburger flex-space-between flex-space-between-header">
+        <div className="hamburger-icon" id="icon">
+          <div className="icon-1" id="a"></div>
+          <div className="icon-2" id="b"></div>
+          <div className="icon-3" id="c"></div>
+          <div className="clear"></div>
+        </div>
+        <nav id="nav">
+          <ul className="flex-space-between-list flex-space-between g-2">
             <li>
               <Link className="nav-link" to={"/"}>
                 Produtos
@@ -36,6 +66,7 @@ export default function Header() {
         <button onClick={logoutAuthContext} className="btnSair btnRed">
           Sair
         </button>
+        {/* <div className="dark-blue" id="blue"></div> */}
       </header>
     </>
   );
