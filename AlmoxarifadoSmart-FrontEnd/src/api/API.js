e7939b8115751110261e2dc775faf5de0146f0c9
Leandro Rocha Santos <lerocha644@gmail.com>
2024-03-17 20:39:21 -0300

    for production v8

--- AlmoxarifadoSmart-FrontEnd/src/api/API.js
+++ AlmoxarifadoSmart-FrontEnd/src/api/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:8010/api",
+  baseURL: "http://3.145.53.73:8010/api",
 });
 //https://localhost:7076/api
 //http://3.145.53.73:8010/api
